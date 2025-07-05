a0d9a983c1bf99ef4cf0190e33f9b8346b232d3f
kolen44 <malavov70@gmail.com>
2025-07-05 15:41:31 +0300

    feat: added ui

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('green-api')
+  getAnimations(): string {
+    const mapDir = join(__dirname, '../static/pages/green-api.html');
+
+    return readFileSync(mapDir, 'utf8');
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GreenApiResolver } from './green-api/green-api.resolver';
 import { GreenApiModule } from './green-api/green-api.module';
 
 @Module({
   imports: [GreenApiModule],
   controllers: [AppController],
-  providers: [AppService, GreenApiResolver],
+  providers: [AppService],
 })
 export class AppModule {}

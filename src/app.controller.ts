da37ba451466be463339fcd2f5c7df260df03d6a
BUILD UP DADDY <126617855+kolen44@users.noreply.github.com>
2025-07-06 02:49:18 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('green-api')
+  @Get()
   getAnimations(): string {
     const mapDir = join(__dirname, '../static/pages/green-api.html');
 
